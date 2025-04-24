f463c62dc1b858cb24d7edf5b257e27631fcb125
Jun Young Kim Lee <jundanielk123@gmail.com>
2025-04-24 12:55:01 -0400

    Update DivyTrips Q1-Q4.sql

--- DivyTrips Q1-Q4.sql
+++ DivyTrips Q1-Q4.sql
@@ -1,24 +1,4 @@
--- Create tables for Q1Q2 and Q3Q4
-CREATE TABLE Q1Q2 (
-    ID INT,
-    name VARCHAR(350),
-    city VARCHAR(100),
-    latitude FLOAT,
-    longitude FLOAT,
-    dpcapacity INTEGER,
-    online_date TIMESTAMP
-);
 
-CREATE TABLE Q3Q4 (
-    ID INT,
-    name VARCHAR(350),
-    city VARCHAR(100),
-    latitude FLOAT,
-    longitude FLOAT,
-    dpcapacity INTEGER,
-    online_date TIMESTAMP,
-    extra_column TEXT 
-);
 
 -- Create and clean Q1 table
 CREATE TABLE Q1 (
@@ -111,10 +91,18 @@ WHERE birthyear = '';
 
 ALTER TABLE Q4
 ALTER COLUMN birthyear TYPE INTEGER USING birthyear::INTEGER;
-
--- Check for null values in Q4
+--Joining tables q1-q4
+Create Table q1_q4_2017 AS
+Select * From q1
+Union All
+Select * From q2
+Union All
+Select * From q3
+Union All
+Select * From q4
+-- Check for null values 
 SELECT *
-FROM Q4
+FROM q1_q4_2017
 WHERE trip_id IS NULL
    OR start_time IS NULL
    OR end_time IS NULL
@@ -142,4 +130,22 @@ FROM q1_q4_2017
 GROUP BY usertype, time_of_day
 ORDER BY time_of_day, total_rides DESC;
 
+-- Checking month to month total rides
+SELECT 
+    DATE_TRUNC('month', start_time) AS month,
+    COUNT(*) AS total_rides
+FROM q1_q4_2017
+GROUP BY month
+ORDER BY month;
+
+-- Trip duration per usertype
+Select sum(tripduration), usertype
+from q1_q4_2017 
+Group by usertype
+
+--Avg ride duration
+SELECT 
+  AVG(EXTRACT(EPOCH FROM (end_time - start_time))) / 60 AS avg_ride_duration_minutes
+FROM 
+  q1_q4_2017
 
